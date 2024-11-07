fe7dd02ed30fb2d46492552f61a592aa81bc5b53
Chris Groves <chris.grover9292@gmail.com>
2024-11-07 14:43:51 -0500

    Updated remote-sample

--- remote-sample/test.py
+++ remote-sample/test.py
@@ -1,4 +1,8 @@
 def main():
+    """Edit this in some way shape or form I don't really care, add it and commit it, make another version, doesn't matter what it is
+    
+    """
+
     
     print("Sharing a new repository")
     
